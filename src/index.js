cddff6128b75d48ced7a88a17ba176dc17bc84aa
pashaSok <https://github.com/pashaSok>
2019-10-19 18:18:53 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+ module.exports = function check(str, bracketsConfig) {
+    var arr = str.split(''),
+    bracketsStack = [],
+    openBrackets = ['{', '(', '['],
+	closeBrackets = ['}', ')', ']'],
+	indexOpenBrackets,indexCloseBrackets;
+
+	for(var j=0; j < arr.length; j++){
+		indexOpenBrackets = openBrackets.indexOf(arr[j]);
+
+		if(indexOpenBrackets != -1){
+			bracketsStack.push(indexOpenBrackets);
+		}
+
+		indexCloseBrackets = closeBrackets.indexOf(arr[j]);
+		if( indexCloseBrackets != -1){
+			indexOpenBrackets = bracketsStack.pop();
+			if(indexCloseBrackets != indexOpenBrackets){
+				return false;
+			}
+		}
+	}
+
+	if(bracketsStack.length !=0){
+		return false;
+	}
+
+    return true;
 }
